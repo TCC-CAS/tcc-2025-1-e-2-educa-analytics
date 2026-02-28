08b792cdaad57faee0f5adaf299591c695c9646d
alineelaine <121210728+alineelaine@users.noreply.github.com>
2026-02-28 17:49:05 -0300

    feat: create API Gateway and update frontend api url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.educa-analytics.com/api',
+  apiUrl: 'https://59oldte6je.execute-api.sa-east-1.amazonaws.com/prod/api',
   version: '1.0.0'
 };
